e671b4419db7cdcf8e644a5404765aeaecd75861
defcarlito <harrisoncamorim@gmail.com>
2025-07-08 19:53:22 -0500

    init ui lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-blue-500">test</div>  
+    <div className="flex w-screen h-screen justify-center">
+      <div className="flex w-full max-w-4xl border-x-2 border-black">
+        
+      </div>
+    </div>  
   );
 }
