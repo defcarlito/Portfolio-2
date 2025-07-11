608022acfb9ce7284f562b97b18b75e2e6d8614a
defcarlito <harrisoncamorim@gmail.com>
2025-07-10 23:40:05 -0500

    styled experience/education

--- src/components/home/history/ExperienceEducation.tsx
+++ src/components/home/history/ExperienceEducation.tsx
@@ -3,17 +3,19 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export default function ExperieceEducation() {
   return (
     <div className="bg-primary-foreground border p-4 shadow-sm rounded-xl w-full max-w-xl">
-      <div className="bg-card border h-fit p-4 rounded-lg shadow-sm">
-        <Tabs defaultValue="experience" className="items-center">
-          <TabsList className="w-full">
-            <TabsTrigger value="experience">Experience</TabsTrigger>
-            <TabsTrigger value="education">Education</TabsTrigger>
-          </TabsList>
+      <div className="h-fit">
+        <Tabs defaultValue="experience" className="items-center flex flex-col gap-4">
+					<div className=" w-full bg-card border p-4 rounded-lg">
+						<TabsList className="w-full">
+							<TabsTrigger value="experience">Experience</TabsTrigger>
+							<TabsTrigger value="education">Education</TabsTrigger>
+						</TabsList>
+					</div>
           <TabsContent value="experience" className="w-full">
-            <div>experience</div>
+            <div className="bg-card shadow-sm border p-4 rounded-lg">experience</div>
           </TabsContent>
           <TabsContent value="education" className="w-full">
-            <div>education</div>
+            <div className="bg-card shadow-sm border p-4 rounded-lg">education</div>
           </TabsContent>
         </Tabs>
       </div>
