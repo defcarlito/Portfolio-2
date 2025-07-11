596ef61fc666104bc37587b637ce2ef2c47885ef
defcarlito <harrisoncamorim@gmail.com>
2025-07-11 13:16:24 -0500

    updated profile hero

--- src/components/home/profile/ProfileHero.tsx
+++ src/components/home/profile/ProfileHero.tsx
@@ -36,17 +36,18 @@ export default function ProfileHero() {
         <div className="bg-card border h-fit py-6 px-12 rounded-lg shadow-sm">
           <div className="flex flex-row gap-6">
             <div>
-              <Avatar className="h-42 w-42 shadow-sm">
+              <Avatar className="h-42 w-42 shadow-sm hover:scale-175 hover:shadow-2xl transofrm transition duration-500">
                 <AvatarImage src="me.jpg" className="scale-300" />
                 <AvatarFallback>Me</AvatarFallback>
               </Avatar>
             </div>
-            <div className="flex flex-col items-start justify-center">
-              <h1 className="text-3xl font-medium">Harrison Amorim</h1>
-              <div className="flex flex-row items-center gap-1 ml-[-2] text-muted-foreground">
-                <MapPin className="w-5 h-5" />
+            <div className="flex flex-col items-start justify-center gap-1">
+              <h1 className="text-2xl font-medium">Hey, I'm Harrison 👋🏼</h1>
+              <div className="flex flex-row items-center gap-1 ml-[-2] text-sm text-muted-foreground">
+                <MapPin className="w-4 h-4" />
                 <p>St. Paul, Minnesota</p>
               </div>
+              <p className="text-md text-muted-foreground">🇺🇸 / 🇧🇷</p>
             </div>
           </div>
         </div>
@@ -66,9 +67,10 @@ export default function ProfileHero() {
             <CardTitle className="text-2xl font-normal">About me</CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-muted-foreground">
+            <p className="text-muted-foreground text-sm">
               I am an incoming 4th year studying Computer Science at the
-              University of Saint Thomas in Saint Paul, Minnesota.
+              University of Saint Thomas in Saint Paul, Minnesota. I was born in
+              the States, then spent some time in São Paulo, Brazil.
             </p>
           </CardContent>
         </Card>
