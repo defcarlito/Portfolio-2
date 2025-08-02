9f47ce00623a3adb883665e2024989dc7c6a25cd
defcarlito <harrisoncamorim@gmail.com>
2025-08-01 23:46:42 -0500

    sync

--- src/components/home/history/school/EducationTimeline.tsx
+++ src/components/home/history/school/EducationTimeline.tsx
@@ -6,7 +6,10 @@ export default function EducationTimeline() {
       <div className="flex flex-row gap-4">
         <div className="flex flex-col w-fit relative">
           <Avatar className="w-10 h-10 rounded-md">
-            <AvatarImage src="ust-logo.png" className="object-contain bg-white p-1"/>
+            <AvatarImage
+              src="ust-logo.png"
+              className="object-contain bg-white p-1"
+            />
             <AvatarFallback>Logo</AvatarFallback>
           </Avatar>
         </div>
@@ -14,7 +17,9 @@ export default function EducationTimeline() {
           <div className="items-center">
             <div className="flex flex-row justify-between items-center">
               <h1 className="font-bold">University of St. Thomas</h1>
-              <h1 className="text-muted-foreground md:text-sm text-xs">Grad - May 2026</h1>
+              <h1 className="text-muted-foreground md:text-sm text-xs">
+                Grad - May 2026
+              </h1>
             </div>
             <div className="flex flex-row justify-between text-sm">
               <h1>BS - Computer Science</h1>
