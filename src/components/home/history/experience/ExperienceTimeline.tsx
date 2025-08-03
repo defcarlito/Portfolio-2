d885a58c7c0c34a2cb7e615f99daa37217b6ee7f
defcarlito <harrisoncamorim@gmail.com>
2025-08-03 11:47:33 -0500

    sync

--- src/components/home/history/experience/ExperienceTimeline.tsx
+++ src/components/home/history/experience/ExperienceTimeline.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 
 export default function ExperienceTimeline() {
